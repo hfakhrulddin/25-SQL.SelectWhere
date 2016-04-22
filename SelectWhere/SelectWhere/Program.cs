f73d132a2de64d7423474a0af32dc7db4a16a12c
Hussen Fakhrulddin <hf556482@gmail.com>
2016-04-22 01:10:20 -0700

    init

---
+++ SelectWhere/SelectWhere/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectWhere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        SELECT* FROM CUSTOMER ;
+        SELECT FIRSTNAME, LASTNAME FROM CUSTOMER;
+
+        SELECT* FROM CUSTOMER, INVOICE ;
+
+            SELECT ID, Name
+FROM Product
+WHERE Name = 'Blade';
+    }
+}
